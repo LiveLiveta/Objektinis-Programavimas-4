2f75fa8dbbae5fccf5125d9254d779b292f945de
Liveta Ruskute <ruskutes@gmail.com>
2025-05-18 21:33:43 +0300

    parasyta funkcija tikZodis

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -4,3 +4,14 @@ bool YraURL(const string& zodis) {
     static const regex url_regex(R"((https?://)?(www\.)?[a-zA-Z0-9\-]+\.(lt|com|org|net|edu|gov|io|co)(/\S*)?)");
     return regex_match(zodis, url_regex);
 }
+
+string tikZodis(const string& zodis) {
+    string rezultatas;
+    for (char ch : zodis) {
+        if (isalpha(ch)) {
+            rezultatas += tolower(ch); // paliekamos tik raidės
+        }
+    }
+    return rezultatas;
+}
+
---
+++ funkcijos.h
@@ -0,0 +1,13 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include <string>
+#include <cctype>
+#include <regex>
+
+using namespace std;
+
+string tikZodis(const string& zodis);
+bool YraURL(const string& zodis);
+
+#endif
