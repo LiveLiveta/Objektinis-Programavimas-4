e4cc480480b969e182e99787c4a989ed6df432dc
Liveta Ruskute <ruskutes@gmail.com>
2025-05-18 21:32:16 +0300

    parasyta funkcija YraURL

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,1 +1,6 @@
 #include <funkcijos.h>
+
+bool YraURL(const string& zodis) {
+    static const regex url_regex(R"((https?://)?(www\.)?[a-zA-Z0-9\-]+\.(lt|com|org|net|edu|gov|io|co)(/\S*)?)");
+    return regex_match(zodis, url_regex);
+}
