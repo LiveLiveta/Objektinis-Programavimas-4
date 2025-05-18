22f04b41c2bff01635406dd8dc9635fae1992971
Liveta Ruskute <ruskutes@gmail.com>
2025-05-18 21:50:11 +0300

    pildytas main.cpp failas, prideta logika

--- main.cpp
+++ main.cpp
@@ -3,6 +3,40 @@
 
 int main() {
 
-    
+    ifstream input("text.txt");
+
+    if (!input.is_open()) {
+        cout << "Nepavyko atidaryti failo text.txt" << endl;
+        return 1;
+    }
+
+    ofstream wordOutput("words.txt");
+    ofstream urlOutput("urls.txt");
+    ofstream xrefOutput("xref.txt");
+
+    unordered_map<string, pair<int, set<int>>> wordInfo;
+    set<string> urls;
+
+    string line;
+    int lineNum = 0;
+
+    while (getline(input, line)) {
+        lineNum++;
+        stringstream ss(line);
+        string word;
+
+        while (ss >> word) {
+            if (YraURL(word)) {
+                urls.insert(word);
+                continue;
+            }
+
+            string tikrasZodis = tikZodis(word);
+            if (!tikrasZodis.empty()) {
+                wordInfo[tikrasZodis].first++;
+                wordInfo[tikrasZodis].second.insert(lineNum);
+            }
+        }
+    }
     return 0;
 }
--- pagalbines.h
+++ pagalbines.h
@@ -1,10 +1,14 @@
-#ifndef COMMON_H
-#define COMMON_H
+#ifndef PAGALBINES_H
+#define PAGALBINES_H
 
 #include <string>
 #include <cctype>
 #include <regex>
-
-using namespace std;
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <set>
+#include <iomanip>
 
 #endif
