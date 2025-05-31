cf4edd25fb7a9b3da2f467acd8b3b90d8ad918e7
Liveta Ruskute <ruskutes@gmail.com>
2025-05-31 23:01:47 +0300

    aprasytos pagalbines.h

---
+++ pagalbines.h
@@ -0,0 +1,20 @@
+#ifndef PAGALBINES_H
+#define PAGALBINES_H
+
+#include <string>
+#include <cctype>
+#include <regex>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <set>
+#include <iomanip>
+
+
+using std::cout;
+using std::runtime_error;
+using std::endl;
+using std::locale;
+
+#endif
