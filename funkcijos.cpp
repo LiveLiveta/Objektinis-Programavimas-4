dbf3065fa485443e7e4eecec08d2740b0c904731
Liveta Ruskute <ruskutes@gmail.com>
2025-05-31 23:00:27 +0300

    aprasytos funkcijos YraURL ir tikZodis

---
+++ funkcijos.cpp
@@ -0,0 +1,18 @@
+#include "funkcijos.h"
+#include "pagalbines.h"
+
+bool YraURL(const string& zodis) {
+    static const regex url_regex(R"((https?://)?(www\.)?[a-zA-Z0-9\-]+\.(lt|com|org|net|edu|gov|io|co)(/\S*)?)");
+    return regex_match(zodis, url_regex);
+}
+
+string tikZodis(const string& zodis) {
+    string rezultatas;
+    for (char ch : zodis) {
+        if (isalpha(ch)) {
+            rezultatas += tolower(ch); // paliekamos tik raidës
+        }
+    }
+    return rezultatas;
+}
+
